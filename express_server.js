fedb21daaaae30ff88e3ba50e4aa625b85aa0ed0
Hilora Dewett <h.dewett@outlook.com>
2022-12-01 19:00:35 -0500

    updated / route to appropriate page based on user's log in status

--- express_server.js
+++ express_server.js
@@ -24,7 +24,12 @@ const users = {};
 
 // GET and POST routes below:
 app.get("/", (req, res) => {
-  res.redirect("/urls");
+  if (req.session["user_id"]){
+    res.redirect("/urls");
+  }
+  else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/urls.json", (req, res) => {
